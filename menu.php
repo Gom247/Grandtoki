dc18dce939ecbdf4895162f8b3417746180160c6
Gom247 <34886358+Gom247@users.noreply.github.com>
2019-03-12 20:48:12 +0700

    penambahan menu

---
+++ menu.php
@@ -0,0 +1,26 @@
+<?php 
+
+include 'connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	
+	$db = new Connection();
+	$list = array();
+	$menu = $db->LihatMenu();
+
+	if ($menu) {
+		foreach ($menu as $row) {
+			array_push($list, array('menu' => $row[1]));
+		}
+	}
+
+	echo ($menu)?
+	json_encode(array("value" => 1, "list" => $list)):
+	json_encode(array("value" => 2, "list" => "gagal"));
+	
+} else {
+	$respone["error"] = true;
+	$respone["message"] = "maintaince";
+	echo json_encode($respone);
+}
+?>
