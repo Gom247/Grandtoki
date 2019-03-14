6b292f460ec483f29ff4f7840173b1feb3118455
Gom247 <34886358+Gom247@users.noreply.github.com>
2019-03-14 18:18:57 +0700

    penambahan layout

--- app/src/main/java/com/example/gom247/grandtoki/MainActivity.java
+++ app/src/main/java/com/example/gom247/grandtoki/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.gom247.grandtoki;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.gom247.grandtoki.api.BaseApiServer;
+import com.example.gom247.grandtoki.api.UtilsApi;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+
+    Context context;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationSelector = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.ngHome:
+                    startActivity(new Intent(context, MainActivity.class));
+                    return true;
+                case R.id.ngKategori:
+                    startActivity(new Intent(context, MainActivity.class));
+                    return true;
+                case R.id.ngAccount:
+                    startActivity(new Intent(context, AccountActivity.class));
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        navigation.setOnNavigationItemSelectedListener(navigationSelector);
+
     }
 }
